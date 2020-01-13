60299b3f31730f1f2e29939bc68ece2802d04b34
Malik Osman <malik@malikosman.com>
2020-01-13 15:21:53 +1100

    deploying to heroku

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_01_13_013750) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.text "name"
     t.text "email"
