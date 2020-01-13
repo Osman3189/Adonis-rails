244d1e7e39058efca599b6ab4f156852bf2cef8d
Malik Osman <malik@malikosman.com>
2020-01-13 16:04:43 +1100

    Training Controller

---
+++ app/controllers/trainings_controller.rb
@@ -0,0 +1,13 @@
+class TrainingsController < ApplicationController
+  def edit
+  end
+
+  def index
+  end
+
+  def new
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  root to: 'welcome#index'
+
 
   get 'session/new'
   get '/login' => 'session#new' # login form
@@ -8,5 +8,7 @@ Rails.application.routes.draw do
     delete '/login' => 'session#destroy' # logout (delete session)
 
     resources :users
+
+    resources :trainings
 
 end
